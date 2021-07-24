a2c6c68f1de9ddb0b80bbc54950dc4b1f7e6f949
Dmitriy Yakovlev <58001401+YakovlevDS@users.noreply.github.com>
2021-07-24 18:59:32 +0300

    added serials

---
+++ js/main.js
@@ -0,0 +1,559 @@
+// Данные для сериалов
+const movies = {
+	witcher: {
+		title: 'Ведьмак',
+		favicon: 'witcher/logo.png',
+		fontColor: '#ffffff',
+		backgroundColor: '#141218',
+		subColor: '#9D2929',
+		background: 'witcher/background.jpg',
+		header: {
+			logo: 'witcher/logo.png',
+			social: [
+				{
+					title: 'Twitter',
+					link: '#',
+					image: 'witcher/social/twitter.svg'
+				},
+				{
+					title: 'Instagram',
+					link: '#',
+					image: 'witcher/social/instagram.svg'
+				},
+				{
+					title: 'Facebook',
+					link: '#',
+					image: 'witcher/social/facebook.svg'
+				}
+			],
+			menu: [
+				{
+					title: 'Описание',
+					link: '#'
+				},
+				{
+					title: 'Трейлер',
+					link: '#'
+				},
+				{
+					title: 'Отзывы',
+					link: '#'
+				}
+			]
+		},
+		main: {
+			genre: '2019, фэнтези',
+			rating: '7',
+			description: 'Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва путешествует по Континенту. За тугой мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.',
+			trailer: 'https://www.youtube.com/watch?v=P0oJqfLzZzQ',
+			slider: [
+				{
+					img: 'witcher/series/series-1.jpg',
+					title: 'Начало конца',
+					subtitle: 'Серия №1'
+				},
+				{
+					img: 'witcher/series/series-2.jpg',
+					title: 'Четыре марки',
+					subtitle: 'Серия №2'
+				},
+				{
+					img: 'witcher/series/series-3.jpg',
+					title: 'Предательская луна',
+					subtitle: 'Серия №4'
+				},
+				{
+					img: 'witcher/series/series-4.jpg',
+					title: 'Банкеты, ублюдки и похороны',
+					subtitle: 'Серия №4'
+				}
+			]
+		},
+		footer: {
+			copyright: '© 2020 Ведьмак. Все права защищены.',
+			menu: [{
+						   title: 'Политика конфиденциальности',
+							 href: ''
+						 },
+						 {
+							 title: 'Условия использования',
+							 href: ''
+						 },
+						 {
+							 title: 'Вернуться',
+							 href: 'index.html'
+						 }
+			]
+		}
+	},
+	lethalweapon: {
+		title: 'Смертельное оружие',
+		favicon: 'lethalweapon/logo.png',
+		fontColor: '#ffffff',
+		backgroundColor: '#141218',
+		subColor: '#5b383c',
+		background: 'lethalweapon/background.jpg',
+		header: {
+			logo: 'lethalweapon/logo.png',
+			social: [
+				{
+					title: 'Twitter',
+					link: '#',
+					image: 'lethalweapon/social/twitter.svg'
+				},
+				{
+					title: 'Instagram',
+					link: '#',
+					image: 'lethalweapon/social/instagram.svg'
+				},
+				{
+					title: 'Facebook',
+					link: '#',
+					image: 'lethalweapon/social/facebook.svg'
+				},
+				{
+					title: 'VK',
+					link: '#',
+					image: 'lethalweapon/social/vk.svg'
+				}
+			],
+			menu: [
+				{
+					title: 'Описание',
+					link: '#'
+				},
+				{
+					title: 'Трейлер',
+					link: '#'
+				},
+				{
+					title: 'Отзывы',
+					link: '#'
+				}
+			]
+		},
+		main: {
+			genre: '2016, боевик, триллер, драма',
+			rating: '7.4',
+			description: 'Сюжет строится на отношениях двух полицейских, ставших напарниками после ряда трагичных ситуаций в жизни каждого. Мартин Риггс и Роджер Мерто хотели бы обойтись без стрессов, но когда полицейская служба была спокойной? Герои борются с преступностью и помогают друг другу решить личные проблемы.',
+			trailer: 'https://www.youtube.com/watch?v=2loYY1f3KCo',
+			slider: [
+				{
+					img: 'lethalweapon/series/series-1.jpg',
+					title: 'Пилот',
+					subtitle: 'Серия №1'
+				},
+				{
+					img: 'lethalweapon/series/series-2.jpg',
+					title: 'Чипсы и соус',
+					subtitle: 'Серия №2'
+				},
+				{
+					img: 'lethalweapon/series/series-3.jpg',
+					title: 'Лучшие приятели',
+					subtitle: 'Серия №4'
+				},
+				{
+					img: 'lethalweapon/series/series-4.jpg',
+					title: 'По соседству',
+					subtitle: 'Серия №4'
+				}
+			]
+		},
+		footer: {
+			copyright: '© 2020 Смертельное оружие. Все права защищены.',
+			menu: [{
+						   title: 'Политика конфиденциальности',
+							 href: ''
+						 },
+						 {
+							 title: 'Условия использования',
+							 href: ''
+						 },
+						 {
+							 title: 'Вернуться',
+							 href: 'index.html'
+						 }
+			]
+		}
+	},
+	loki: {
+		title: 'Локи',
+		favicon: 'loki/logo.png',
+		fontColor: '#ffffff',
+		backgroundColor: '#141218',
+		subColor: '#129e23',
+		background: 'loki/background.jpg',
+		header: {
+			logo: 'loki/logo.png',
+			social: [
+				{
+					title: 'Twitter',
+					link: '#',
+					image: 'loki/social/twitter.svg'
+				},
+				{
+					title: 'Instagram',
+					link: '#',
+					image: 'loki/social/instagram.svg'
+				},
+				{
+					title: 'Facebook',
+					link: '#',
+					image: 'loki/social/facebook.svg'
+				}
+			],
+			menu: [
+				{
+					title: 'Описание',
+					link: '#'
+				},
+				{
+					title: 'Трейлер',
+					link: '#'
+				},
+				{
+					title: 'Отзывы',
+					link: '#'
+				}
+			]
+		},
+		main: {
+			genre: '2021, боевик, фэнтези, приключения',
+			rating: '7.9',
+			description: 'Локи попадает в таинственную организацию «Управление временными изменениями» после того, как он украл Тессеракт, и путешествует во времени, меняя историю.',
+			trailer: 'https://www.youtube.com/watch?v=YrjHcYqe31g',
+			slider: [
+				{
+					img: 'loki/series/series-1.jpg',
+					title: 'Славная миссия',
+					subtitle: 'Серия №1'
+				},
+				{
+					img: 'loki/series/series-2.jpg',
+					title: 'Вариант',
+					subtitle: 'Серия №2'
+				},
+				{
+					img: 'loki/series/series-3.jpg',
+					title: 'Ламентис',
+					subtitle: 'Серия №4'
+				},
+				{
+					img: 'loki/series/series-4.jpg',
+					title: 'Событие Нексуса',
+					subtitle: 'Серия №4'
+				}
+			]
+		},
+		footer: {
+			copyright: '© 2020 Локи. Все права защищены.',
+			menu: [{
+						   title: 'Политика конфиденциальности',
+							 href: ''
+						 },
+						 {
+							 title: 'Условия использования',
+							 href: ''
+						 },
+						 {
+							 title: 'Вернуться',
+							 href: 'index.html'
+						 }
+			]
+		}
+	},
+}
+
+// Название сериала из GET параметра
+let serialName = window.location.search.split('=')[1];
+
+// Функция создания элемента
+const getElement = (tagName, classNames, attributes) => {
+	// Создание заданного элемента
+	const element = document.createElement(tagName);
+	// Добавление классов созданному элементу
+	if (classNames) {
+		element.classList.add(...classNames);
+	}
+	// Добавление дополнительных свойств при создании элемента
+	if (attributes) {
+		for (const attribute in attributes) {
+			element[attribute] = attributes[attribute];
+		}
+	}
+
+	return element;
+}
+
+// Функция создания хэдера со всеми элементами
+const createHeader = ({ title, header: { logo, social, menu } }) => {
+	const header = getElement('header');
+	const container = getElement('div', ['container']);
+	const headerWrapper = getElement('div', ['header']);
+	const headerMenuButton = getElement('button', ['menu-button'])
+
+	// Создание и вставка логотипа
+	if (logo) {
+		const logoImg = getElement('img', ['logo'], {
+			src: logo,
+			alt: 'Логотип ' + title
+		});
+		headerWrapper.append(logoImg);
+	}
+
+	// Создание и добавление меню
+	if (menu) {
+		const nav = getElement('nav', ['menu-list']);
+		const allMenus = menu.map(item => {
+			const menuLink = getElement('a', ['menu-link'], {
+				href: item.link,
+				textContent: item.title
+			})
+
+			return menuLink;
+		});
+
+		nav.append(...allMenus);
+		headerWrapper.append(nav);
+	}
+
+	// Создание и вставка колонки соцсетей
+	if (social) {
+		const socialWrapper = getElement('div', ['social']);
+		const allSocial = social.map(item => {
+			const socialLink = getElement('a', ['social-link']);
+			socialLink.append(getElement('img', ['social-link-img'], {
+				src: item.image,
+				alt: item.title,
+			}))
+			socialLink.href = item.link;
+
+			return socialLink;
+		});
+
+		socialWrapper.append(...allSocial);
+		headerWrapper.append(socialWrapper);
+	}
+
+	header.append(container);
+	container.append(headerWrapper);
+	container.append(headerMenuButton);
+
+	// Открытие меню при клике на бургер
+	headerMenuButton.addEventListener('click', () => {
+		headerMenuButton.classList.toggle('menu-button-active');
+		headerWrapper.classList.toggle('header-active');
+	})
+
+	return header;
+};
+
+// Функция создания секции main со всеми элементами
+const createMain = ({ title, main: { genre, rating, description, trailer, slider } }) => {
+	const main = getElement('main'),
+				container = getElement('div', ['container']),
+				mainContent = getElement('div', ['main-content']),
+				content = getElement('div', ['content']);
+	
+	// Строка с жанром
+	if (genre) {
+		const genreSpan = getElement('span', ['genre', 'animated', 'fadeInRight'], {textContent: genre});
+		content.append(genreSpan);
+	}
+
+	// Звездочки рейтинга
+	if (rating) {
+		const ratingBlock = getElement('div', ['rating', 'animated', 'fadeInRight']),
+					ratingStars = getElement('div', ['rating-stars']),
+					ratingNumber = getElement('div', ['rating-number'], {textContent: `${rating}/10`});
+		
+		for (let i = 0; i < 10; i++) {
+			const star = getElement('img', ['star'], {
+				alt: i ? '' : `Рейтинг: ${rating} из 10`,
+				src: !i || i < rating ? 'img/star.svg' : 'img/star-o.svg'
+			})
+			ratingStars.append(star);
+		}
+
+		ratingBlock.append(ratingStars, ratingNumber)
+		content.append(ratingBlock);
+	}
+
+	// Создание и вставка заголовка сайта
+	content.append(getElement('h1',
+														['main-title', 'animated', 'fadeInRight'],
+														{textContent: title}));
+
+	// Создание блока с описанием фильма
+	if (description) {
+		content.append(getElement('p',
+															['main-description', 'animated', 'fadeInRight'],
+															{textContent: description}));
+	}
+
+	// Создание блока с трейлером
+	if (trailer) {
+		const yuotubeLink = getElement('a',
+																	 ['button', 'animated', 'fadeInRight', 'youtube-modal'],
+																	 {
+																		 href: trailer,
+																		 textContent: 'Смотреть трейлер'
+																	 });
+
+		const youtubeImgLink = getElement('a',
+																		 ['play', 'youtube-modal'],
+																		 {
+																			 href: trailer,
+																			 ariaLabel: 'Смотреть трейлер'
+																			});
+		const iconPlay = getElement('img',
+																['play-img'],
+																{
+																	alt: 'play',
+																	src: 'img/play.svg',
+																	ariaHidden: true
+																})
+
+		content.append(yuotubeLink);
+		youtubeImgLink.append(iconPlay);
+		mainContent.append(youtubeImgLink);
+	}
+
+	// Создание слайдера с сериями
+	if (slider) {
+		const sliderSeries = getElement('div', ['series']),
+					swiperContainer = getElement('div', ['swiper-container']),
+					swiperWrapper = getElement('div', ['swiper-wrapper']),
+					arrowBtn = getElement('button', ['arrow']);
+
+		const slides = slider.map(item => {
+			// Создание карточки для каждого слайда
+			const swiperSlide = getElement('div', ['swiper-slide']),
+						card = getElement('figure', ['card']),
+						cardImage = getElement('img', ['card-img'], {
+							src: item.img,
+							alt: ((item.title ? item.title + ' ' : '') + (item.subtitle ? item.subtitle : '')).trim()
+						});
+
+			card.append(cardImage);
+			// Заголовок и подзаголовок для карточки
+			if (item.title || item.subtitle) {
+				const cardDescription = getElement('figcaption', ['card-description']);
+				cardDescription.innerHTML = (item.subtitle ? '<p class="card-subtitle">' + item.subtitle + '</p>' : '') + (item.title ? '<p class="card-title">' + item.title + '</p>' : '');
+
+				card.append(cardDescription);
+			}
+			swiperSlide.append(card);
+
+			return swiperSlide;
+		});
+		
+		swiperWrapper.append(...slides);
+		swiperContainer.append(swiperWrapper);
+		sliderSeries.append(swiperContainer, arrowBtn);
+		container.append(sliderSeries);
+
+		// Активация слайдера
+		new Swiper(swiperContainer, {
+			loop: true,
+			navigation: {
+				nextEl: arrowBtn,
+			},
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 20
+				},
+				541: {
+					slidesPerView: 2,
+					spaceBetween: 40
+				}
+			}
+		});
+	}
+
+	
+	mainContent.append(content);
+	container.append(mainContent);
+
+	main.append(container);
+
+	return main;
+}
+
+// Функция создания футера со всеми элементами
+const createFooter = ({ footer: {copyright, menu} }) => {
+	const footer =  getElement('footer', ['footer']),
+				container = getElement('div', ['container']),
+				footerContent = getElement('div', ['footer-content']),
+				footerLeftSection = getElement('div', ['left']),
+				footerRightSection = getElement('div', ['right']),
+				footerMenu = getElement('nav', ['footer-menu']);
+
+	footerLeftSection.append(getElement('span', ['copyright'], {textContent: copyright}));
+
+	menu.forEach(item => {
+		footerMenu.append(getElement('a',
+																 ['footer-link'],
+																 {
+																	 href: item.href,
+																	 textContent: item.title,
+																	}))
+	});
+	footerRightSection.append(footerMenu);
+
+	footerContent.append(footerLeftSection, footerRightSection);
+	container.append(footerContent);
+	footer.append(container);
+
+	return footer;
+}
+
+const movieConstructor = (selector, options) => {
+	// Смена title страницы сайта
+	document.title = options.title;
+	// Установка фавиконки
+	if (options.favicon) {
+		const index = options.favicon.lastIndexOf('.');
+		const type = options.favicon.substring(index + 1);
+		
+		const favicon = getElement('link', null, {
+																								rel: 'icon',
+																								href: options.favicon,
+																								type: 'image/' + (type === 'svg' ? 'svg-xml' : type)
+																							});
+
+		document.head.append(favicon);
+	}
+
+	const app = document.querySelector(selector);
+	app.classList.add('body-app');
+	// Вставка фоновой картинки, если она есть
+	app.style.backgroundImage = options.background ? `url('${options.background}')` : '';
+	// Смена цвета шрифта
+	app.style.color = options.fontColor || '';
+	// Смена фонового цвета
+	app.style.backgroundColor = options.backgroundColor || '';
+	// Смена дополнительной цветовой схемы
+	if (options.subColor) document.documentElement.style.setProperty('--sub-color', options.subColor);
+
+	// Проверка наличия вводных данных для шапки сайта
+	if (options.header) {
+		// Формирование хэдера и его дальнейшая вставка
+		app.append(createHeader(options));
+	}
+	// Проверка наличия вводных данных для главной секции
+	if (options.main) {
+		// Формирование хэдера и его дальнейшая вставка
+		app.append(createMain(options));
+	}
+	// Проверка наличия вводных данных для подвала сайта
+	if (options.footer) {
+		// Формирование хэдера и его дальнейшая вставка
+		app.append(createFooter(options));
+	}
+};
+
+// 
+movieConstructor('.app', movies[serialName]);
